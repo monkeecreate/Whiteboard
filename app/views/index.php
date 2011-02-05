a368557e4769f9cbf5b40b45c04590c4e5bb82c9
John Hoover <defvayne23@gmail.com>
2011-02-04 18:16:38 -0600

    Add testing for sort saving, and delete.

--- app/views/index.php
+++ app/views/index.php
@@ -4,35 +4,38 @@ $this->loadView("inc_header.php");
 ?>
 	
 	<section id="columns">
+		<div id="delete" class="topRight">
+			<img src="/images/delete.png"> Delete
+		</div>
 		<section class="mockup columns">
 			<h2>Mockup</h2>
 			<ul style="color: #ffa549;">
-				<li>Lorem Ipsum Website</li>
-				<li>Lorem Ipsum Mobile Site</li>
+				<li data-id="1"><img src="/images/pan.png" class="move"> Lorem Ipsum Website</li>
+				<li data-id="2"><img src="/images/pan.png" class="move"> Lorem Ipsum Mobile Site</li>
 			</ul>
 		</section>
 		
 		<section class="coding columns">
 			<h2>Coding</h2>
 			<ul style="color: #1fbdf8;">
-				<li>Lorem Ipsum Project</li>
-				<li>Lorem Ipsum Mobile Project</li>
+				<li data-id="3"><img src="/images/pan.png" class="move"> Lorem Ipsum Project</li>
+				<li data-id="4"><img src="/images/pan.png" class="move"> Lorem Ipsum Mobile Project</li>
 			</ul>
 		</section>
 		
 		<section class="content columns">
 			<h2>Content</h2>
 			<ul style="color: #e74af5;">
-				<li>Lorem Ipsum App</li>
-				<li>Lorem Ipsum Mobile App</li>
+				<li data-id="5"><img src="/images/pan.png" class="move"> Lorem Ipsum App</li>
+				<li data-id="6"><img src="/images/pan.png" class="move"> Lorem Ipsum Mobile App</li>
 			</ul>
 		</section>
 		
 		<section class="launch columns">
 			<h2>Launch</h2>
 			<ul style="color: #00d54f;">
-				<li>Lorem Ipsum Web App</li>
-				<li>Lorem Ipsum Mobile Web App</li>
+				<li data-id="7"><img src="/images/pan.png" class="move"> Lorem Ipsum Web App</li>
+				<li data-id="8"><img src="/images/pan.png" class="move"> Lorem Ipsum Mobile Web App</li>
 			</ul>
 		</section>
 		
--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,34 @@
 $(function() {		
-	$( ".columns ul" ).sortable({
+	$(".columns ul").sortable({
+		cursorAt: 'top',
+		handle: '.move',
 		placeholder: "item-highlight",
-		connectWith: ".columns ul"
+		connectWith: ".columns ul",
+		start: function(event, ui) {
+			$("#delete").fadeIn();
+		},
+		stop: function(event, ui) {
+			$("#delete").fadeOut();
+			console.log("Save order.");
+		}
 	}).disableSelection();
+	
+	$(".columns ul").draggable({
+		helper: 'clone',
+		cursor: 'move'
+	});
+	
+	$("#delete").droppable({
+		over: function() {
+			console.log("Over");
+		},
+		out: function() {
+			console.log("Out");
+		},
+		drop: function(event, ui) {
+			// $(".columns ul").sortable('cancel');
+			$(ui.draggable).remove();
+			console.log("Delete project.");
+		}
+	});
 });
